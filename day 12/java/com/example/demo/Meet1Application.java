1acc16c3bc0937cdad3d9286e8257d0a3f5a09b5
Balaji_krishov <110535593+Balaji-krish09@users.noreply.github.com>
2023-10-09 10:44:25 +0530

    Add files via upload

---
+++ day 12/java/com/example/demo/Meet1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meet1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meet1Application.class, args);
+	}
+
+}
